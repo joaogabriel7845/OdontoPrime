facc6668f451b2943e0a6e2cfa7de1053ae350a1
Dogcore <aquinojoao373@gmail.com>
2026-06-08 11:26:50 -0300

    NavItem animado

--- src/components/NavItem.jsx
+++ src/components/NavItem.jsx
@@ -1,6 +1,6 @@
 function NavItem({ label }) {
   return (
-    <p className="transition-all text-gray-300 hover:cursor-pointer hover:text-white border-b border-transparent hover:border-lavender font-[Inter]">
+    <p className="transition-all text-gray-300 hover:cursor-pointer hover:text-white border-b-2 border-transparent hover:border-lavender hover:-translate-y-1 font-[Inter]">
       {label}
     </p>
   );
